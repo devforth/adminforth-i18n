4bfed20ca81aa0124df144ec9b9eb11b50834eab
yaroslav8765 <pechorkin2014@gmail.com>
2026-03-09 10:08:57 +0200

    fix: add margin-top to the main container in TranslationJobViewComponent
    https://web.tracklify.com/project/2b7ZVgE5/AdminForth/1288/V3F96xOb/translation-progress

--- custom/TranslationJobViewComponent.vue
+++ custom/TranslationJobViewComponent.vue
@@ -1,6 +1,6 @@
 
 <template>
-    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 my-3">
+    <div class="grid grid-cols-1 sm:grid-cols-2 gap-4 my-3 mt-9">
       <div class="flex items-center space-x-1">
         <span class=" text-gray-500">{{ t('Total tokens will be used for translation:') }}</span>
         <span class="text-lg font-semibold text-gray-900 dark:text-gray-100">{{ new Number(props.job.state?.totalTranslationTokenCost).toLocaleString() || 0 }}</span>
